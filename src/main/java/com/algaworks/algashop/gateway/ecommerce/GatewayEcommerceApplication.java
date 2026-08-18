cdb48f60e02bbd1f95b39e62c602817c9c5bda65
William Gomes <william.desenv@gmail.com>
2026-08-18 10:25:12 -0400

    Update

---
+++ src/main/java/com/algaworks/algashop/gateway/ecommerce/GatewayEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algashop.gateway.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayEcommerceApplication.class, args);
+	}
+
+}
